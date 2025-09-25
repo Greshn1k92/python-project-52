50b1ea164d7cd789c9a82d46a675285c0ec7e08b
Greshnik <sanmaximus92@mail.ru>
2025-09-25 09:37:00 +0300

    Заменить форму TaskForm на улучшенную версию с явным определением полей

--- task_manager/tasks/forms.py
+++ task_manager/tasks/forms.py
@@ -1,23 +1,75 @@
 from django import forms
-from django.contrib.auth.models import User
-from .models import Task
+from django.utils.translation import gettext_lazy as _
+from task_manager.labels.models import Label
+from task_manager.statuses.models import Status
+from task_manager.tasks.models import Task
+from django.contrib.auth import get_user_model
 
+User = get_user_model()
 
 class TaskForm(forms.ModelForm):
-    class Meta:
-        model = Task
-        fields = ['name', 'description', 'status', 'executor', 'labels']
-        widgets = {
-            'name': forms.TextInput(attrs={'class': 'form-control'}),
-            'description': forms.Textarea(attrs={'class': 'form-control', 'rows': 3}),
-            'status': forms.Select(attrs={'class': 'form-control'}),
-            'executor': forms.Select(attrs={'class': 'form-control'}),
-            'labels': forms.CheckboxSelectMultiple(attrs={'class': 'form-check-input'})
-        }
 
     def __init__(self, *args, **kwargs):
         super().__init__(*args, **kwargs)
-        self.fields['executor'].queryset = User.objects.all()
-        self.fields['executor'].required = False
-        # Добавляем пустую опцию в начало списка
+        self.fields['description'].required = False
+        self.fields['labels'].required = False
+        # Добавляем пустую опцию для executor
         self.fields['executor'].empty_label = "---------"
+        self.fields['executor'].required = False
+
+    status = forms.ModelChoiceField(
+        queryset=Status.objects.all(),
+        label=_('Status'),
+        required=True,
+        widget=forms.Select(attrs={'class': 'form-select form-select-sm'})
+    )
+
+    executor = forms.ModelChoiceField(
+        queryset=User.objects.all(),
+        label=_('Executor'),
+        required=False,  # Изменено на False
+        widget=forms.Select(attrs={'class': 'form-select form-select-sm'})
+    )
+
+    labels = forms.ModelMultipleChoiceField(
+        queryset=Label.objects.all(),
+        widget=forms.SelectMultiple(attrs={
+            'class': 'form-select form-select-sm'
+        }),
+        label=_('Labels')
+    )
+
+    def clean_name(self):
+        name = self.cleaned_data.get('name')
+
+        if hasattr(self, 'instance') and self.instance.pk:
+            if Task.objects.filter(name=name).exclude(
+                    pk=self.instance.pk).exists():
+                raise forms.ValidationError(
+                    _("Task with this name already exists."))
+        else:
+            if Task.objects.filter(name=name).exists():
+                raise forms.ValidationError(
+                    _("Task with this name already exists."))
+
+        return name
+
+    class Meta:
+        model = Task
+        fields = ["name", "description", "status", "executor", "labels"]
+        labels = {
+            'name': _('Name'),
+            'description': _('Description'),
+        }
+        widgets = {
+            'name': forms.TextInput(attrs={
+                'placeholder': _('Name'),
+                'class': 'form-control'
+                }
+            ),
+            'description': forms.Textarea(attrs={
+                'placeholder': _('Description'),
+                'class': 'form-control'
+                }
+            ),
+        }
