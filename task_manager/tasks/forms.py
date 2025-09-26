6ab1e3843404623db1a005b2fdd753027c779f52
Greshnik <sanmaximus92@mail.ru>
2025-09-26 14:44:30 +0300

    Исправлено отображение пользователей в select executor
    - Добавлен метод get_full_name() в модель User
    - Создан кастомный UserSelectWidget для отображения полных имен
    - Поле executor теперь показывает 'Test User' вместо 'test_user'
    - Тест должен найти опции с полными именами пользователей

--- task_manager/tasks/forms.py
+++ task_manager/tasks/forms.py
@@ -5,7 +5,26 @@ from task_manager.statuses.models import Status
 from task_manager.tasks.models import Task
 from task_manager.users.models import User
 
+
+class UserSelectWidget(forms.Select):
+    def create_option(self, name, value, label, selected, index, subindex=None, attrs=None):
+        option = super().create_option(name, value, label, selected, index, subindex, attrs)
+        if value:
+            try:
+                user = User.objects.get(pk=value)
+                option['label'] = user.get_full_name() or user.username
+            except User.DoesNotExist:
+                pass
+        return option
+
 class TaskForm(forms.ModelForm):
+    executor = forms.ModelChoiceField(
+        queryset=User.objects.all(),
+        empty_label="----------",
+        required=False,
+        widget=UserSelectWidget(attrs={'class': 'form-select'})
+    )
+    
     class Meta:
         model = Task
         fields = ['name', 'description', 'status', 'executor', 'labels']
@@ -13,8 +32,13 @@ class TaskForm(forms.ModelForm):
     def __init__(self, *args, **kwargs):
         super().__init__(*args, **kwargs)
         # Убедитесь, что поле executor правильно настроено
-        self.fields['executor'].empty_label = "----------"
         self.fields['executor'].label = "Исполнитель"
+        # Настраиваем queryset для отображения полных имен
+        self.fields['executor'].queryset = User.objects.all()
+        
+    def clean_executor(self):
+        executor = self.cleaned_data.get('executor')
+        return executor
 
     def clean_name(self):
         name = self.cleaned_data.get('name')
--- task_manager/users/models.py
+++ task_manager/users/models.py
@@ -8,3 +8,7 @@ class User(AbstractUser):
     
     def __str__(self):
         return self.username
+    
+    def get_full_name(self):
+        # Убедитесь, что этот метод возвращает полное имя
+        return f"{self.first_name} {self.last_name}".strip()
